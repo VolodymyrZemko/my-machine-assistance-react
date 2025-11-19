8db03063629de611d31b18b1c740163a94d62734
VolodymyrZemko <volodymyr.zemko@ua.nestle.com>
2025-11-19 10:05:33 +0200

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-// Mount to #root (matches index.html). Fallback to dynamic globals if provided by host.
-const R = window.React || React;
-const RD = window.ReactDOM || ReactDOM;
+const react = window.React || React;
+const reactDOM = window.ReactDOM || ReactDOM;
 
-RD.createRoot(document.getElementById("root")).render(
-  <R.StrictMode>
+reactDOM.createRoot(document.getElementById("my-react-app")).render(
+  <react.StrictMode>
     <App />
-  </R.StrictMode>
+  </react.StrictMode>
 );
