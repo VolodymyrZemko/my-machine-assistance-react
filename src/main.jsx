240aed1185245f213fdf12a403caa5ab7c7665d0
VolodymyrZemko <volodymyr.zemko@ua.nestle.com>
2025-11-19 11:29:34 +0200

    v2.2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css"; // global styles (root, body, fonts)
 import "./App.css"; // app layout & component styles
 
 const react = window.React || React;
