f1497cbf5dca40139c43efa34fd9b30e87856872
VolodymyrZemko <volodymyr.zemko@ua.nestle.com>
2025-11-19 14:46:09 +0200

    GH pages changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  return {
+    plugins: [react()],
+    base: isProd ? '/my-react-cms-app/' : '/', // GitHub Pages base path
+  };
 });
