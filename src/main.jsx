e1ab71991f39db34eae3ab79bc57dd7c91c1b1d9
VolodymyrZemko <volodymyr.zemko@ua.nestle.com>
2025-11-20 10:13:21 +0200

    test5

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,6 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { HashRouter } from 'react-router-dom';
-// import './index.css'; // global styles (fonts, background)
-// import App from "./App.jsx";
-
-// const react = window.React || React;
-// const reactDOM = window.ReactDOM || ReactDOM;
-
-// reactDOM.createRoot(document.getElementById("my-react-app")).render(
-//   <react.StrictMode>
-//     <HashRouter>
-//       <App />
-//     </HashRouter>
-//   </react.StrictMode>
-// );
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from 'react-router-dom';
 import './index.css'; // global styles (fonts, background)
 import App from "./App.jsx";
 
@@ -26,6 +9,23 @@ const reactDOM = window.ReactDOM || ReactDOM;
 
 reactDOM.createRoot(document.getElementById("my-react-app")).render(
   <react.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </react.StrictMode>
 );
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import './index.css'; // global styles (fonts, background)
+// import App from "./App.jsx";
+
+// const react = window.React || React;
+// const reactDOM = window.ReactDOM || ReactDOM;
+
+// reactDOM.createRoot(document.getElementById("my-react-app")).render(
+//   <react.StrictMode>
+//     <App />
+//   </react.StrictMode>
+// );
