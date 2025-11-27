6f23c57de848ddee8ca89d33099082e5b7e0e83d
VolodymyrZemko <volodymyr.zemko@ua.nestle.com>
2025-11-27 13:01:33 +0200

    Updates

--- src/data/machines.js
+++
@@ -1,7 +0,0 @@
-import machinesData from './machines.json';
-
-export function getMachinesByTechnology(tech) {
-  return machinesData.filter(m => m.technology === tech);
-}
-
-export { machinesData };
